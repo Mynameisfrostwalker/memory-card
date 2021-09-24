9273f3e53b682acfc1175cb59164d90dbc38ed60
Chibuzor Okoroafor <chibuzorokoroafor6@gmail.com>
2021-09-24 11:18:28 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+## Memory card game
+
+Memory card game for **TOP**
---
+++ src/App.js
@@ -0,0 +1,6 @@
+import React from "react";
+import "./css/App.css";
+
+const App = () => <div className="App" />;
+
+export default App;

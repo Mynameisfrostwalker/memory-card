65fbf4e42e2b8df1fab902b2c70e322455876c38
Chibuzor Okoroafor <chibuzorokoroafor6@gmail.com>
2021-09-25 17:50:35 +0100

    Added children components to App

--- src/App.js
+++ src/App.js
@@ -1,6 +1,15 @@
 import React from "react";
 import "./css/App.css";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import Main from "./components/Main";
 
-const App = () => <div className="App" />;
+const App = () => (
+  <div>
+    <Header />
+    <Main />
+    <Footer />
+  </div>
+);
 
 export default App;

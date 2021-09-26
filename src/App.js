64d4c3daad1a4a87d751a783e1c3f040a92cf136
Chibuzor Okoroafor <chibuzorokoroafor6@gmail.com>
2021-09-26 14:33:16 +0100

    added jsdoc

--- src/App.js
+++ src/App.js
@@ -4,6 +4,14 @@ import Header from "./components/Header";
 import Footer from "./components/Footer";
 import Main from "./components/Main";
 
+/**
+ * @module App
+ */
+
+/**
+ * App component
+ * @return {JSX}
+ */
 const App = () => (
   <div>
     <Header />
